6c26c67010307da92385e4dc58885cccccbca3d5
Ansh339412 <hakikatsingh099@gmail.com>
2024-09-16 11:07:04 -0400

    first commit

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -18,7 +18,20 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
-
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     # <HINT> Create a route for show_exam_result view
+    def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+    total_score = 0
+    for choice in choices:
+        if choice.is_correct:
+            total_score += choice.question.grade
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
 
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
